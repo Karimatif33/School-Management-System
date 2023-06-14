009fff3b1f610fde9ac42e3160a9c41c692af03a
Karimatif33 <karimatif33@gmail.com>
2023-06-14 22:15:50 +0300

    add dbConnect from config with .env

--- server/config/dbConnect.js
+++ server/config/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require ("mongoose");
 // mongoose.set("strictQuery", false);
 const dbConnect =async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL)
+        await mongoose.connect(process.env.MONGO_URL || 'mongodb+srv://karimatif:123456**@cluster0.ryo5x4h.mongodb.net/ssm')
         console.log("DB Conected Successfully");
     } catch (err) {
         console.log("DB Conection failed", err.message);
